e2409e9eea031736ef8cf006ad6112e20e03e48b
dannyeiei <danvittipat@gmail.com>
2022-10-03 13:33:17 +0700

    startup

---
+++ sec_news.php
@@ -0,0 +1,70 @@
+<section data-bs-version="5.1" class="gallery3 cid-thoSKWvT44" id="gallery3-19">
+    <div class="container-fluid">
+        <div class="mbr-section-head">
+            <h4 class="mbr-section-title mbr-fonts-style align-center mb-0 display-2">
+                <strong>ข่าวประชาสัมพันธ์</strong></h4>
+            
+        </div>
+        <div class="row mt-4">
+            <div class="item features-image сol-12 col-md-6 col-lg-3">
+                <div class="item-wrapper">
+                    <div class="item-img">
+                        <a href="page7.html"><img src="assets/images/2.jpg" alt="Mobirise Website Builder"></a>
+                    </div>
+                    <div class="item-content">
+                        <h5 class="item-title mbr-fonts-style display-7">
+                            <strong>หัวข้อข่าว</strong></h5>
+                        
+                        <p class="mbr-text mbr-fonts-style mt-3 display-7">
+                            รายละเอียดข่าวย่อ................................. <a href="#" class="text-primary">Read more..</a></p>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="item features-image сol-12 col-md-6 col-lg-3">
+                <div class="item-wrapper">
+                    <div class="item-img">
+                        <img src="assets/images/s-27336711.jpg" alt="Mobirise Website Builder">
+                    </div>
+                    <div class="item-content">
+                        <h5 class="item-title mbr-fonts-style display-7">
+                            <strong>หัวข้อข่าว</strong></h5>
+                        
+                        <p class="mbr-text mbr-fonts-style mt-3 display-7">
+                            รายละเอียดข่าวย่อ............................... <a href="#" class="text-primary">Read more..</a></p>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="item features-image сol-12 col-md-6 col-lg-3">
+                <div class="item-wrapper">
+                    <div class="item-img">
+                        <img src="assets/images/3.jpg" alt="Mobirise Website Builder">
+                    </div>
+                    <div class="item-content">
+                        <h5 class="item-title mbr-fonts-style display-7">
+                            <strong>หัวข้อข่าว</strong></h5>
+                        
+                        <p class="mbr-text mbr-fonts-style mt-3 display-7">
+                            รายละเอียดข่าวย่อ.............................. <a href="#" class="text-primary">Read more..</a></p>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="item features-image сol-12 col-md-6 col-lg-3">
+                <div class="item-wrapper">
+                    <div class="item-img">
+                        <img src="assets/images/4.jpg" alt="Mobirise Website Builder" title="">
+                    </div>
+                    <div class="item-content">
+                        <h5 class="item-title mbr-fonts-style display-7"><strong>หัวข้อข่าว</strong></h5>
+                        
+                        <p class="mbr-text mbr-fonts-style mt-3 display-7">
+                            รายละเอียดข่าวย่อ........................... <a href="#" class="text-primary">Read more..</a></p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
